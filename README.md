bc1a787cdccce05d347c9895a9569fcf7621216a
Andrey Gershun <agershun@gmail.com>
2014-11-13 20:35:46 +0300

    I lost two hours of work...

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Alasql.js - pure JavaScript client-side in-memory fast SQL-database 
 
-Version: 0.0.16 Date: November 13, 2014 [Change log](CHANGELOG.md), [Release plan](RELEASES.md)
+Version: 0.0.17 Date: November 13, 2014 [Change log](CHANGELOG.md), [Release plan](RELEASES.md)
 
 Alasql - '[à la SQL](http://en.wiktionary.org/wiki/%C3%A0_la)' - is a lightweight client-side in-memory SQL database designed to work in browser and Node.js. 
 
--- alasql.js
+++ alasql.js
@@ -4236,6 +4236,61 @@ yy.AlterTable.prototype.compile = function (db) {
 
 /*
 //
+// CREATE TABLE for Alasql.js
+// Date: 03.11.2014
+// (c) 2014, Andrey Gershun
+//
+*/
+
+yy.CreateIndex = function (params) { return yy.extend(this, params); }
+yy.CreateTable.prototype.toString = function() {
+	var s = 'CREATE';
+	if(this.unique) s+=' UNIQUE';
+	s += ' INDEX ' + this.indexid + " ON "+this.table.toString();
+	s += "("+this.columns.toString()+")";
+	return s;
+}
+
+// CREATE TABLE
+yy.CreateIndex.prototype.compile = function (db) {
+//	var self = this;
+	var tableid = this.table.tableid;
+	var unique = this.unique;
+	var indexid = this.indexid;
+
+	return function() {
+		return 0;
+	};
+};
+
+
+
+
+/*
+//
+// DROP TABLE for Alasql.js
+// Date: 03.11.2014
+// (c) 2014, Andrey Gershun
+//
+*/
+
+yy.DropIndex = function (params) { return yy.extend(this, params); }
+yy.DropIndex.prototype.toString = function() {
+	return 'INDEX TABLE' + this.indexid;
+}
+
+
+// DROP TABLE
+yy.DropIndex.prototype.compile = function (db) {
+	var indexid = this.indexid;
+	return function() {
+		return 1;
+	}
+};
+
+
+/*
+//
 // INSERT for Alasql.js
 // Date: 03.11.2014
 // (c) 2014, Andrey Gershun
