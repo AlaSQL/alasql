c389db4b1802a421c64771bfbce42f1cce8c0302
Andrey Gershun <agershun@gmail.com>
2014-12-07 17:33:11 +0300

    IndexDB tests

--- test/test155.js
+++ test/test155.js
@@ -7,97 +7,14 @@ if(typeof exports === 'object') {
 
 if(typeof exports != 'object') {
 
-describe('Test 154 - InsexedDB test', function() {
+describe('Test 155 - InsexedDB INSERT', function() {
 
-	it("1. Create Database and Table", function(done){
-		alasql('DROP IndexedDB DATABASE IF EXISTS ag154', [], function(res){
-			assert(res == 1 || res == 0);
-			alasql('CREATE IndexedDB DATABASE ag154', [], function(res){
-				assert(res == 1);
-				alasql('SHOW IndexedDB DATABASES', [], function(res) {
-					var found = false;
-					res.forEach(function(d){
-						found = found || d.databaseid == 'ag154';
-					});
-					assert(found);
-					alasql('ATTACH IndexedDB DATABASE ag154', [], function(res){
-						assert(res == 1);
-						alasql('CREATE TABLE ag154.one', [], function(res){
-							assert(res == 1);
-							alasql('SHOW TABLES FROM ag154', [], function(res){
-								assert(res.length == 1);
-								assert(res[0].tableid == 'one');
-									// console.log(997,res);
-
-								alasql('DROP TABLE ag154.one', [], function(res){
-									// console.log(998,res);
-									assert(res == 1);
-									alasql('SHOW TABLES FROM ag154', [], function(res){
-										// console.log(999,res);
-										assert(res.length == 0);
-		console.log(alasql.databases.ag154);
-
-										done();
-
-									});
-								});
-							});
-						});
-					});
-				});
-			});
-		});
-	});
-
-	it("2. Show Databases Async", function(done){
-		alasql('SHOW IndexedDB DATABASES', [], function(res){
-			// console.log(100,res);
-			done();
-		});
-	});
-
-	it("3. Show Databases Like Async", function(done){
-		alasql('SHOW IndexedDB DATABASES LIKE "my%"', [], function(res){
-			// console.log(200, res);
-		});
-		done();
-	});
-
-	it("4. Drop Database", function(done){
-		// console.log(alasql.databases.ag154);
-		alasql('DROP IndexedDB DATABASE IF EXISTS ag154', [], function(res){
-			assert(res == 1);
-//			console.log(alasql.databases);
-			alasql('DROP DATABASE IF EXISTS ag154', [], function(res){
-				assert(res == 1 || res == 0);
-				alasql('SHOW IndexedDB DATABASES', [], function(res){
-					// console.log(300,res);
-					done();
-				});
-			});
-		});
-	});
-
-	it("5. Multiple lines async", function(done){
-		alasql('CREATE IndexedDB DATABASE ag154a;SHOW IndexedDB DATABASES;DROP IndexedDB DATABASE ag154a', [], function(res){
-			assert(res[0] == 1);
-			var found = false;
-			res[1].forEach(function(d){
-				found = found || d.databaseid == 'ag154a';
-			});
-			assert(found);
-			assert(res[2] == 1);
-//		 	console.log(900, res);
-			done();
-		});
-	});
-
-	it("6. Multiple lines async", function(done){
-		alasql('DROP IndexedDB DATABASE IF EXISTS ag154b;'+
-			'CREATE IndexedDB DATABASE ag154b;'+
-			'ATTACH IndexedDB DATABASE ag154b AS test154b;'+
-			'CREATE TABLE test154b.one;'+
-			'INSERT INTO test154b.one VALUES @{a:10};'+
+	it("1. Multiple lines async", function(done){
+		alasql('DROP IndexedDB DATABASE IF EXISTS ag155;'+
+			'CREATE IndexedDB DATABASE ag155;'+
+			'ATTACH IndexedDB DATABASE ag155 AS test155;'+
+			'CREATE TABLE test155.one;'+
+			'INSERT INTO test155.one VALUES @{a:10};'+
 			'', [], function(res){
 		 	console.log(950, res);
 			done();
