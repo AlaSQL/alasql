29066ad319d9bcb9ef891356607ccdafc01b65e7
Andrey Gershun <agershun@gmail.com>
2015-03-03 16:07:36 +0300

    test243

--- test/test243.js
+++ test/test243.js
@@ -8,7 +8,7 @@ if(typeof exports === 'object') {
 
 describe('Test 243 AVG bug', function() {
 
-  it('1. AVG bug',function(done){
+  it('1. AVG bug: 3 groups with one and multiple item',function(done){
 
       var arr = [
             {
@@ -28,13 +28,17 @@ describe('Test 243 AVG bug', function() {
 
     var res = alasql('SELECT person, avg(sold) FROM ? WHERE 1 GROUP BY person',[arr]);
 
-    console.log(res);
-//      assert(data[0].CV == 100);
+//    console.log(res);
+      assert(res,
+        [ { person: 1, 'AVG(sold)': 12.5 },
+          { person: 2, 'AVG(sold)': 10 },
+          { person: 3, 'AVG(sold)': 40 } ]
+        );
       done();
   });
 
 
-  it('1. AVG bug',function(done){
+  it('2. AVG 2 by 2 groups',function(done){
 
       var arr = [
             {
@@ -54,8 +58,11 @@ describe('Test 243 AVG bug', function() {
 
     var res = alasql('SELECT person, avg(sold) FROM ? WHERE 1 GROUP BY person',[arr]);
 
-    console.log(res);
-//      assert(data[0].CV == 100);
+//    console.log(res);
+      assert(res,
+      [ { person: 1, 'AVG(sold)': 12.5 },
+        { person: 2, 'AVG(sold)': 25 } ]
+        );
       done();
   });
 
