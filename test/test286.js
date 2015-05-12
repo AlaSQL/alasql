fa4ce6419594efccfd06e4c4441abe0ced5b7c94
Andrey Gershun <agershun@gmail.com>
2015-05-12 23:21:00 +0300

    test

--- test/test286.js
+++ test/test286.js
@@ -19,9 +19,20 @@ describe('Test 286 CREATE UNIQUE INDEX', function() {
   });
   it('2. Fill tables with data',function(done){
     this.timeout(100000);
-    //for 
-    var M = [(Math.random()*30)|0,(Math.random()*30)|0,(Math.random()*30)|0,(Math.random()*30)|0,(Math.random()*30)|0];
-    console.log(M,test(M));
+    
+    var K = 100; // Number of runs
+    var P = 20; // Number of records coefficient
+
+    var L = 0  // Number of successful runs
+    for(var k = 0; k<K; k++) {
+	    var M = [(Math.random()*P+1)|0,(Math.random()*P+1)|0,(Math.random()*P+1)|0,(Math.random()*P+1)|0,(Math.random()*P+1)|0];
+	    var R = test(M);
+	    //console.log(M,R, (M[1]*M[2]>M[3]*M[4])==(R[0]>R[1]));
+
+	    // Hypothesis
+	    if((M[1]*M[2]>M[3]*M[4])==(R[0]>R[1])) L++;
+	};
+	console.log(L/K); // Probablity
     done();
   });
   it('3. DROP DATABASE',function(done){
@@ -31,6 +42,11 @@ describe('Test 286 CREATE UNIQUE INDEX', function() {
 });
 
 function test(M){
+	alasql('DELETE FROM one;');
+	alasql('DELETE FROM two;');
+	alasql('DELETE FROM three;');
+	alasql('DELETE FROM four;');
+
     for(var i=0;i<M[0];i++) {
       for(var j=0;j<M[1];j++) {
         alasql('INSERT INTO one VALUES ?',[{a:i, b:j}]);
@@ -50,9 +66,9 @@ function test(M){
     alasql.databases[alasql.useid].dbversion++;
     var tm1 = Date.now();
     var res1 = alasql('SELECT * FROM one \
-      JOIN two ON one.b = two.b \
-      JOIN three ON two.c = three.c \
-      JOIN four ON three.d = four.d \
+      INNER JOIN two ON one.b = two.b \
+      INNER JOIN three ON two.c = three.c \
+      INNER JOIN four ON three.d = four.d \
       ');
     var tm1 = Date.now() - tm1;
 
@@ -62,12 +78,15 @@ function test(M){
     var tm2 = Date.now();
     var res2 = alasql('SELECT * \
       FROM four \
-      JOIN three ON three.d = four.d \
-      JOIN two ON two.c = three.c \
-      JOIN one ON one.b = two.b \
+      INNER JOIN three ON three.d = four.d \
+      INNER JOIN two ON two.c = three.c \
+      INNER JOIN one ON one.b = two.b \
       ');
     var tm2 = Date.now() - tm2;
-    if(res1.length != res2.length) throw new Error('Different results');
+    if(res1.length != res2.length) {
+    	console.log(M,res1.length,res2.length);
+    	throw new Error('Different results');
+    }
     return [tm1,tm2];
 };
 
