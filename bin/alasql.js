e0ee37ff6cad8263662ba13816c5d6728b36ef7c
Mathias Rangel Wulff <mathias@rawu.dk>
2015-07-28 10:54:13 +0200

    Removed hardcoded value
    Wuups...

--- bin/alasql.js
+++ bin/alasql.js
@@ -18,20 +18,20 @@ var fs = require('fs');
  * @returns {Boolean}
  */
 
-function isDirectory(filePath) {
-  var isDir = false;
-  try {
-    var absolutePath = path.resolve(filePath);
-    isDir = fs.lstatSync(absolutePath).isDirectory();
-  } catch (e) {
-    isDir = e.code === 'ENOENT';
-  }
-  return isDir;
+function isDirectory(filePath){
+      	var isDir = false;
+      	try {
+      	      	var absolutePath = path.resolve(filePath);
+      	      	isDir = fs.lstatSync(absolutePath).isDirectory();
+      	} catch (e) {
+      	      	isDir = e.code === 'ENOENT';
+      	}
+      	return isDir;
 }
 
 
 if(process.argv.length <= 2) {
-	console.log('AlaSQL command-line utility (version '+alasql.version+') ');
+	console.log('AlaSQL command-line utility (version '+alasql.version+')');
 	console.log();
 	console.log('Usage:');
 	console.log('  alasql "sql-statement" [ params ]        - Run SQL statement');
@@ -74,7 +74,7 @@ if(sql === '-f' || sql === '--file' ) {
 		process.exit(1);
 	}
 
-	sql = fs.readFileSync('/Users/mrw/git/slet/file.sql', 'utf8').toString();
+	sql = fs.readFileSync(filePath, 'utf8').toString();
 	parami++;
 }
 
@@ -93,17 +93,16 @@ for(var i=parami;i<process.argv.length;i++) {
 
 
 
- alasql
- 	.promise(sql,params)
-    .then(function(res){
-      	if(!alasql.options.stdout){
+ alasql.promise(sql,params)
+      	.then(function(res){
+      	      	if(!alasql.options.stdout){
 			console.log(res);
 		}
 		process.exit(0);
-    }).catch(function(err){
-        console.log(err);
-        process.exit(1);
-    });
+      	}).catch(function(err){
+      	      	console.log(err);
+      	      	process.exit(1);
+      	});
 
 
 
