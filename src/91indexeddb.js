AlaSQL/alasql

commit 656792d46c0050e5cdd51e5361904915b64017b6

Fix alasql for running in cordova on iOS. #410

src/91indexeddb.js
@@ -6,7 +6,7 @@
 //
 
 
- if(typeof window !='undefined' && typeof window.indexedDB != 'undefined') {
+ if(window !='undefined' && window.indexedDB) {
 
 var IDB = alasql.engines.INDEXEDDB = function (){};
 

dist/alasql.js
@@ -16595,7 +16595,7 @@ WEBSQL.attachDatabase = function(databaseid, dbid, args, params, cb){
 //
 
 
- if(typeof window !='undefined' && typeof window.indexedDB != 'undefined') {
+ if(typeof window !='undefined' && window.indexedDB) {
 
 var IDB = alasql.engines.INDEXEDDB = function (){};
 

commit 7b0f4676dcd91b6b29e0d999d72a7324f45890bb

Fixed bug introduced in 656792d46c0050e5cdd51e5361904915b64017b6

src/91indexeddb.js
@@ -6,7 +6,7 @@
 //
 
 
- if(window !='undefined' && window.indexedDB) {
+ if(typeof(window) != 'undefined' && window.indexedDB) {
 
 var IDB = alasql.engines.INDEXEDDB = function (){};
 