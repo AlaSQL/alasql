eb409edf297dcf53cac8ed8c434c1eb8d151c3fe
Andrey Gershun <agershun@gmail.com>
2015-12-05 22:10:35 +0300

    Fixed test and FLOAT type

--- src/59convert.js
+++ src/59convert.js
@@ -132,8 +132,6 @@ alasql.stdfn.CONVERT = function(value, args) {
 		s += " "+("0"+d.getHours()).substr(-2)+":"+("0"+d.getMinutes()).substr(-2)+":"+("0"+d.getSeconds()).substr(-2);
 		s += '.'+("00"+d.getMilliseconds()).substr(-3)
 		return s;
-	} else if(['NUMBER','FLOAT'].indexOf(udbtypeid)>-1) {
-		return +val;
 	} else if(['MONEY'].indexOf(udbtypeid)>-1) {
 		var m = +val;
 		return (m|0)+((m*100)%100)/100;
@@ -147,7 +145,15 @@ alasql.stdfn.CONVERT = function(value, args) {
 	} else if(['CHAR','CHARACTER', 'NCHAR'].indexOf(udbtypeid)>-1) {
 		return (val+(new Array(args.dbsize+1).join(" "))).substr(0,args.dbsize);
 		//else return ""+val.substr(0,1);
-	} else if(['DECIMAL','NUMERIC'].indexOf(udbtypeid)>-1) {
+	} else if(['NUMBER','FLOAT'].indexOf(udbtypeid)>-1) {
+		if(typeof args.dbprecision != 'undefined') {
+			var m = +val;
+			var fxd = Math.pow(10,args.dbprecision);
+			return (m|0)+((m*fxd)%fxd)/fxd;
+		} else {
+			return +val;
+		}
+	} else if((['DECIMAL','NUMERIC'].indexOf(udbtypeid)>-1)) {
 		var m = +val;
 		var fxd = Math.pow(10,args.dbprecision);
 		return (m|0)+((m*fxd)%fxd)/fxd;
@@ -156,6 +162,8 @@ alasql.stdfn.CONVERT = function(value, args) {
 		try {
 			return JSON.parse(val);
 		} catch(err) { throw new Error('Cannot convert string to JSON');};
+	} else {
+		throw new Error('Wrong conversion type');
 	};
 	return val;
 };
