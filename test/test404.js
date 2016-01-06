dfb6f10c6fde38d078895bb333d13c0f068c61db
Andrey Gershun <agershun@gmail.com>
2016-01-06 20:29:41 +0300

    OUTER JOIN test

---
+++ test/test404.js
@@ -0,0 +1,40 @@
+if(typeof exports === 'object') {
+	var assert = require("assert");
+	var alasql = require('..');
+	var DOMStorage = require("dom-storage");
+//	global.localStorage = new DOMStorage("./test390.json", { strict: false, ws: '' });
+};
+
+/*
+ This sample beased on SQLLOGICTEST
+*/
+
+describe('Test 404 OUTER JOIN', function() {
+
+  it('1. CREATE DATABASE',function(done){
+    alasql('CREATE DATABASE test404;USE test404');
+    done();
+  });
+
+  var data = {
+      COLORS: [{id:1,name:'red'},{id:2,name:'blue'},{id:3,name:'orange'}],            
+      FRUITS:[{id:1,name:'apple'},{id:2,name:'grape'},{id:3,name:'orange'}],
+      MASCOTS:[{id:1,name:'redsox'},{id:2,name:'whitesox'},{id:3,name:'orange'}]
+  };
+
+	it('2. Create table and index before insert', function(done){
+    var res = alasql('select t0.name t0n ,t1.name t1n, t2.name t2n from ? t0 outer join ? t1 on t1.name = t0.name outer join ? t2 on t2.name = t0.name or t2.name = t1.name',[data.COLORS,data.FRUITS, data.MASCOTS]);
+    console.log(res);
+    done();
+  });
+
+
+
+
+  it('99. DROP DATABASE',function(done){
+    alasql('DROP DATABASE test404');
+    done();
+  });
+
+
+});
