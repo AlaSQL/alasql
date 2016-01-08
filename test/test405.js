6eb924c74be866f0538b8cf895eb84458df03873
Andrey Gershun <agershun@gmail.com>
2016-01-08 15:38:45 +0300

    Removed test405.js

--- test/test405.js
+++ test/test405.js
@@ -9,7 +9,7 @@ if(typeof exports === 'object') {
  This sample beased on SQLLOGICTEST
 */
 
-describe('Test 405. IS NULL strange parser bug', function() {
+describe('Test 405. ...', function() {
 
   it('1. CREATE DATABASE',function(done){
     alasql('CREATE DATABASE test405;USE test405');
@@ -18,16 +18,10 @@ describe('Test 405. IS NULL strange parser bug', function() {
 
 
 	it('2. Parse SQL', function(done){
-      var res = alasql.parse('ROW OF SELECT NULL IS NOT NULL, 1 IS NOT NULL, NULL IS NULL, 1 IS NOT NULL');
-      assert(res.statements[0].columns.length == 4);
-      done();
-    });
 
-  it('2. Execute SQL', function(done){
-      var res = alasql('ROW OF SELECT NULL IS NOT NULL, 1 IS NOT NULL, NULL IS NULL, 1 IS NOT NULL');
-      //assert(res.length == 4);
       done();
-    });
+  });
+
   it('99. DROP DATABASE',function(done){
     alasql('DROP DATABASE test405');
     done();
