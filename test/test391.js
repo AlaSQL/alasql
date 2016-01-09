458ac6d090fc184dbd882b40d4ee58176ec93371
Andrey Gershun <agershun@gmail.com>
2016-01-09 22:07:06 +0300

    test390.json removed

--- test/test391.js
+++ test/test391.js
@@ -1,8 +1,8 @@
 if(typeof exports === 'object') {
 	var assert = require("assert");
 	var alasql = require('..');
-	var DOMStorage = require("dom-storage");
-	global.localStorage = new DOMStorage("./test390.json", { strict: false, ws: '' });
+//	var DOMStorage = require("dom-storage");
+//	global.localStorage = new DOMStorage("./test390.json", { strict: false, ws: '' });
 };
 
 /*
--- test/test396.js
+++ test/test396.js
@@ -1,7 +1,7 @@
 if(typeof exports === 'object') {
 	var assert = require("assert");
 	var alasql = require('..');
-	var DOMStorage = require("dom-storage");
+//	var DOMStorage = require("dom-storage");
 //	global.localStorage = new DOMStorage("./test390.json", { strict: false, ws: '' });
 };
 
