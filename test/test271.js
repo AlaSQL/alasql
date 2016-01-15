fba44f37709d18e7421e60baf0b0d4451dd3c466
Mathias Rangel Wulff <m@rawu.dk>
2016-01-15 21:47:34 +0100

    Updated tests to lowdas 4

--- test/test271.js
+++ test/test271.js
@@ -1,17 +1,21 @@
 if(typeof exports === 'object') {
 	var assert = require("assert");
 	var alasql = require('..');
-  var _ = require('lodash');
 } else {
 	__dirname = '.';
 };
 
 
 describe('Test 271 RECORDSET and Excel tests', function() {
+	
+	// missing in lodash 4
+	var pluck = function(arr, key) { 
+		return arr.map(function(e) { return e[key]; }) 
+	}
 
   it('1. Open Excel and columns', function(done) {
     var res = alasql('SELECT RECORDSET * FROM XLSX("test168.xlsx",{headers:true})',[],function(res){
-    var colres = _.pluck(res.columns,"columnid");
+    var colres = pluck(res.columns,"columnid");
       assert.deepEqual(colres,["City","Population"]);
       done();    
     });  
