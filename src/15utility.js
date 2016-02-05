eb76fb698e8c9068b1ae0345308e7c8b5e41b34c
Mathias Rangel Wulff <m@rawu.dk>
2016-02-05 16:51:21 +0000

    Better Browserify detection

--- src/15utility.js
+++ src/15utility.js
@@ -206,7 +206,7 @@ utils.isBrowser = isBrowser();
     @return {boolean} True if code is running in a browser with a browserify setup
 */
 var isBrowserify = function(){
-	return utils.isBrowser && (typeof exports === 'object');
+	return utils.isBrowser && utils.global.process && utils.global.process.browser
 }
 utils.isBrowserify = isBrowserify();
 
