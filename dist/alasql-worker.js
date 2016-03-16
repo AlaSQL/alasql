a3d410c980a2c153561e419c5147de79f81c9137
Mathias Rangel Wulff <m@rawu.dk>
2016-03-16 21:15:53 +0100

    Promise chain of queries

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.2.4-develop-1238 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
+//! AlaSQL v0.2.4-develop-1241 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
 /*
 @module alasql
-@version 0.2.4-develop-1238
+@version 0.2.4-develop-1241
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
