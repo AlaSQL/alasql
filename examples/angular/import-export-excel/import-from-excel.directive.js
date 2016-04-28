338bbc428f455524b28af97a87169a3fb56cacc7
Mathias Rangel Wulff <m@rawu.dk>
2016-04-28 12:20:56 +0200

    Create import-from-excel.directive.js

---
+++ examples/angular/import-export-excel/import-from-excel.directive.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('jfy')
+        .directive('importFromExcel', importFromExcel);
+
+    function importFromExcel(ImportExportToExcel) {
+        var directive = {
+            restrict: 'A',
+            link: linkFunc
+        };
+        return directive;
+
+        function linkFunc(scope, element) {
+            element.change(function (event) {
+                ImportExportToExcel.importFromExcel(event.originalEvent);
+            });
+        }
+    }
+})();
