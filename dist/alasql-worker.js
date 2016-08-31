cc5074b07b874052c39f417d289855cea7daffba
Bob <bob-de-ruiter@live.nl>
2016-08-31 20:25:09 +0200

    (null = null) is null, baby.
    * Implement three-valued logic
    
    * Now also for unary operators
    
    * Add tests
    
    * dist

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.3.1-develop-1389 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
+//! AlaSQL v0.3.1-3valued-1396 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
 /*
 @module alasql
-@version 0.3.1-develop-1389
+@version 0.3.1-3valued-1396
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
