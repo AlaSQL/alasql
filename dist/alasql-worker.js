b4394ac67c3baf4a2ac90d6b3f1736eae6647186
root <bob-de-ruiter@live.nl>
2016-09-06 06:39:23 -0400

    dist

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.3.2-develop-1425 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
+//! AlaSQL v0.3.2-value-1408 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
 /*
 @module alasql
-@version 0.3.2-develop-1425
+@version 0.3.2-value-1408
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
