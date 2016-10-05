a88c77d2d616f933f1a7c01e2eac3bc7b1fc9688
seb-ster <srvanheerde@gmail.com>
2016-10-05 22:50:16 +0200

    Issue #762 added TRUNCATE to Local Storage Engine
    Added LS.truncateTable method to Local Storage Engine

---
+++ test/test607.js
@@ -0,0 +1,26 @@
+if(typeof exports === 'object') {
+	var assert = require("assert");
+	var alasql = require('..');
+}
+
+var test = '607'; // insert test file number
+
+describe('Test '+test+' - TRUNCATE on Local Storage table', function() {
+
+	before(function(){
+		alasql('create LOCAL STORAGE database test'+test);
+		alasql('ATTACH LOCALSTORAGE DATABASE test'+test);
+		alasql('use test'+test);
+		alasql('create table one (a int)');
+		alasql('insert into one values (1),(2),(3),(4),(5)');
+	});
+
+	after(function(){
+		alasql('drop database test'+test);
+	});
+
+	it('A) attempt TRUNCATE on table', function(){
+		var res = alasql('TRUNCATE TABLE one (a int)');
+		assert.equal(res, 1);
+	});
+});
