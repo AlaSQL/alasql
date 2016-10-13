b881f3a7effc217fb316230132f2db4e1b0ed881
Mathias Rangel Wulff <m@rawu.dk>
2016-10-13 23:24:44 +0200

    Updated version in files to 0.3.3
    код не волк - в лес не убежит.

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.3.2-develop-1428 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
+//! AlaSQL v0.3.3 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
 /*
 @module alasql
-@version 0.3.2-develop-1428
+@version 0.3.3
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
