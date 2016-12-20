8857b52ca7da0b12eae846eaea043e15dc4d78f0
Kang Hong Jin <kanghongjin@gmail.com>
2016-12-21 00:33:59 +0800

    dist

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.3.4-develop-1458 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
+//! AlaSQL v0.3.4-793-1459 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
 /*
 @module alasql
-@version 0.3.4-develop-1458
+@version 0.3.4-793-1459
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
