b8954b145f77116f642abb5dd4bc3e6107a4aefa
Kang HJ <kanghongjin@gmail.com>
2017-03-26 11:09:59 +0800

    remove commented out code

--- test/test152.js
+++ test/test152.js
@@ -57,8 +57,6 @@ describe('Test 152 - INSERT/DELETE/UPDATE for localStorage with AUTOCOMMIT', fun
 		
 		alasql('CREATE TABLE IF NOT EXISTS ls152.three (a int AUTO_INCREMENT, b string)');
 		alasql('INSERT INTO ls152.three (b) VALUES ("Rome"),("London"),("Berlin"),("Paris")');
-//		console.log(alasql.databases.ls152.tables.one.data);
-//		assert(!alasql.databases.ls152.tables.one.data);
 
 		var res = alasql('SELECT * FROM ls152.three');
 		assert.deepEqual(res, [{"a":1,"b":"Rome"},{"a":2,"b":"London"},{"a":3,"b":"Berlin"},{a:4,b:'Paris'}]);
