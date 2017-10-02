9971346c15d451a31e3fa57a93d3d4d58cc72cef
Mathias Rangel Wulff <m@rawu.dk>
2017-10-02 22:30:46 +0200

    Support test of 0.12

--- dist/alasql.js
+++ dist/alasql.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.4.2-develop-1554 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
+//! AlaSQL v0.4.2-develop-1555 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT 
 /*
 @module alasql
-@version 0.4.2-develop-1554
+@version 0.4.2-develop-1555
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
@@ -137,7 +137,7 @@ var alasql = function(sql, params, cb, scope) {
 	Current version of alasql 
  	@constant {string} 
 */
-alasql.version = '0.4.2-develop-1554';
+alasql.version = '0.4.2-develop-1555';
 
 /**
 	Debug flag
