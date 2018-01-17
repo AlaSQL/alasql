283d5226174d11a1eefc9535dded954e8eb5ff8f
Alex Peattie <alexpeattie@gmail.com>
2018-01-17 11:39:40 +0000

    Fix missing ORDER BY direction when calling toString on AST

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.4.4-develop-1566 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT
+//! AlaSQL v0.4.4-develop-1567 | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT
 /*
 @module alasql
-@version 0.4.4-develop-1566
+@version 0.4.4-develop-1567
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
