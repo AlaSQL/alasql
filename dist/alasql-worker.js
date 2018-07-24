3f996ea8eaf774f380a069d493518e3ea27c70a2
Eli Palmer <eli@harperdb.io>
2018-07-24 08:25:49 -0600

    refactored to remove async/await for older version CI testing

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.4.8-alasql-1030-4bc5f776undefined | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT
+//! AlaSQL v0.4.8-alasql-1030-4753fc6fundefined | © 2014-2016 Andrey Gershun & Mathias Rangel Wulff | License: MIT
 /*
 @module alasql
-@version 0.4.8-alasql-1030-4bc5f776undefined
+@version 0.4.8-alasql-1030-4753fc6fundefined
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
