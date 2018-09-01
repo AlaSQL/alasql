c79a5fa55f3c79cf8e42f354ed3c8d43a3903164
alex <alex@generia.de>
2018-09-02 01:24:38 +0200

    fix for creating xlsxml ouput with multiple sheets

--- test/test280.js
+++ test/test280.js
@@ -50,15 +50,32 @@ if (typeof exports == 'object') {
 		});
 
 		it('3. Save complex XLSXML', function(done) {
-			alasql(
-				'SELECT * INTO XLSXML("' +
-					__dirname +
-					'/restest280c.xls",{headers:true, sheets:{Sheet1:{},Sheet2:{}}}) FROM ?',
-				[data],
-				function() {
+			var outfile = __dirname + '/restest280c.xls';
+			var data2 = [
+				{pet: 'dog', legs: 4},
+				{pet: 'bird', legs: 2}
+			];
+			alasql('SELECT * INTO XLSXML(?,{headers:true, sheets:{Sheet1:{},Sheet2:{}}}) FROM ?', [outfile, [data, data2]], function() {
+				alasql('SEARCH XML Worksheet %[ss:Name] FROM XML(?)', [outfile], function(res) {
+					assert.deepEqual(res, ['Sheet1', 'Sheet2']);
+					alasql('SEARCH XML / * Data$ FROM XML(?)', [outfile], function(res) {
+						assert.equal(res.length, 12);
+						done();
+					});
+				});
+			});
+		});
+
+		it('4. Save XLSXML with headers array', function(done) {
+			var outfile = __dirname + '/restest280d.xls';
+			alasql('SELECT * INTO XLSXML(?,{headers: ?}) FROM ?', [outfile, ["City", "Population"], data], function() {
+				alasql('SEARCH XML / * Data$ FROM XML(?)', [outfile], function(res) {
+					assert.equal(res.length, 10);
+					assert.deepEqual(res[0], 'City');
+					assert.deepEqual(res[1], 'Population');
 					done();
-				}
-			);
+				});
+			});
 		});
 	});
 }
