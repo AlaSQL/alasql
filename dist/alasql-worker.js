53a7c528388787014cc969b7b41c3cf98fdfd96b
Mathias Rangel Wulff <m@rawu.dk>
2020-01-29 04:57:58 +0100

    Better typing. Fix #1138

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -1,7 +1,7 @@
-//! AlaSQL v0.5.4 | © 2014-2018 Andrey Gershun & Mathias Rangel Wulff | License: MIT
+//! AlaSQL v0.5.4-develop-4ffa0c71undefined | © 2014-2018 Andrey Gershun & Mathias Rangel Wulff | License: MIT
 /*
 @module alasql
-@version 0.5.4
+@version 0.5.4-develop-4ffa0c71undefined
 
 AlaSQL - JavaScript SQL database
 © 2014-2016	Andrey Gershun & Mathias Rangel Wulff
