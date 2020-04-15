00a030c48959031e683efd6f087d03c919068e9f
Ramnath <kiran.ramnath@sage.com>
2020-04-15 21:34:18 +0200

    prettier

--- dist/alasql-worker.js
+++ dist/alasql-worker.js
@@ -38,16 +38,15 @@ SOFTWARE.
 //
 */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        root.alasql = factory();
-    }
-}(this, function () {
-
-/**
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	} else {
+		root.alasql = factory();
+	}
+})(this, function () {
+	/**
 	Main procedure for worker
     @function
     @param {string} sql SQL statement
@@ -55,191 +54,186 @@ SOFTWARE.
     @param {callback} cb Callback function
     @return {object} Query result
 */
-function alasql(sql,params,cb){
+	function alasql(sql, params, cb) {
+		params = params || [];
 
-	params = params||[];
+		// Avoid setting params if not needed even with callback
+		if (typeof params === 'function') {
+			scope = cb;
+			cb = params;
+			params = [];
+		}
 
-	// Avoid setting params if not needed even with callback
-	if(typeof params === 'function'){
-		scope = cb;
-		cb = params;
-		params = [];
-	}
-
-	if(typeof params !== 'object'){
+		if (typeof params !== 'object') {
 			params = [params];
+		}
+
+		// Increase last request id
+		var id = alasql.lastid++;
+		// Save callback
+		alasql.buffer[id] = cb;
+		// Send a message to worker
+		alasql.webworker.postMessage({id: id, sql: sql, params: params});
 	}
 
-    // Increase last request id
-	var id = alasql.lastid++;
-    // Save callback
-	alasql.buffer[id] = cb;
-    // Send a message to worker
-	alasql.webworker.postMessage({id:id,sql:sql,params:params});
-}
+	alasql.options = {};
+	alasql.options.progress = function () {};
 
-
-alasql.options = {};
-alasql.options.progress = function(){};
-
-isArray = function(obj){
-	return "[object Array]"===Object.prototype.toString.call(obj);
-}
-
-alasql.promise = function() {
-	throw new Error('Please include a Promise/A+ library');
-}
-
-// From src/18promise.js
-if(typeof Promise !== "undefined"){
-	var promiseExec = function(sql, params, counterStep, counterTotal){
-		 return new Promise(function(resolve, reject){
-	        alasql(sql, params, function(data,err) {
-	             if(err) {
-	                 reject(err);
-	             } else {
-					if (counterStep && counterTotal && alasql.options.progress !== false) {
-						alasql.options.progress(counterStep, counterTotal);
-					}
-	                resolve(data);
-	             }
-	        });
-	    });
-	}
-
-	var promiseAll = function(sqlParamsArray){
-		if(sqlParamsArray.length<1){
-			return ;
-		}
-
-		var active, sql, params;
-
-		var execArray = []; 
-
-		for (var i = 0; i < sqlParamsArray.length; i++) {
-			active = sqlParamsArray[i];
-
-			if(typeof active === 'string'){
-				active = [active];
-			}
-
-			if(!isArray(active) || active.length<1 || 2<active.length){
-				throw new Error('Error in .promise parameter');
-			}
-
-			sql = active[0];
-			params = active[1]||undefined;
-
-			execArray.push(promiseExec(sql, params, i, sqlParamsArray.length));
-		}
-
-		return Promise.all(execArray); 
-	}
-
-	alasql.promise = function(sql, params) {
-		if(typeof Promise === "undefined"){
-			throw new Error('Please include a Promise/A+ library');
-		}
-
-		if(typeof sql === 'string'){
-			return promiseExec(sql, params);
-		}
-
-		if(!isArray(sql) || sql.length<1 || typeof params !== "undefined"){
-			throw new Error('Error in .promise parameters');
-		}
-		return promiseAll(sql);
+	isArray = function (obj) {
+		return '[object Array]' === Object.prototype.toString.call(obj);
 	};
 
-}
+	alasql.promise = function () {
+		throw new Error('Please include a Promise/A+ library');
+	};
 
+	// From src/18promise.js
+	if (typeof Promise !== 'undefined') {
+		var promiseExec = function (sql, params, counterStep, counterTotal) {
+			return new Promise(function (resolve, reject) {
+				alasql(sql, params, function (data, err) {
+					if (err) {
+						reject(err);
+					} else {
+						if (counterStep && counterTotal && alasql.options.progress !== false) {
+							alasql.options.progress(counterStep, counterTotal);
+						}
+						resolve(data);
+					}
+				});
+			});
+		};
 
-alasql = alasql || false;
+		var promiseAll = function (sqlParamsArray) {
+			if (sqlParamsArray.length < 1) {
+				return;
+			}
 
-if (!alasql) {
-	throw new Error('alasql was not found');
-}
+			var active, sql, params;
 
-alasql.worker = function() {
-	throw new Error('Can find webworker in this enviroment');
-};
+			var execArray = [];
 
-if (typeof Worker !== 'undefined') {
-	alasql.worker = function(path, paths, cb) {
-		//	var path;
-		if (path === true) {
-			path = undefined;
-		}
+			for (var i = 0; i < sqlParamsArray.length; i++) {
+				active = sqlParamsArray[i];
 
-		if (typeof path === 'undefined') {
-			var sc = document.getElementsByTagName('script');
-			for (var i = 0; i < sc.length; i++) {
-				if (sc[i].src.substr(-16).toLowerCase() === 'alasql-worker.js') {
-					path = sc[i].src.substr(0, sc[i].src.length - 16) + 'alasql.js';
-					break;
-				} else if (sc[i].src.substr(-20).toLowerCase() === 'alasql-worker.min.js') {
-					path = sc[i].src.substr(0, sc[i].src.length - 20) + 'alasql.min.js';
-					break;
-				} else if (sc[i].src.substr(-9).toLowerCase() === 'alasql.js') {
-					path = sc[i].src;
-					break;
-				} else if (sc[i].src.substr(-13).toLowerCase() === 'alasql.min.js') {
-					path = sc[i].src.substr(0, sc[i].src.length - 13) + 'alasql.min.js';
-					break;
+				if (typeof active === 'string') {
+					active = [active];
+				}
+
+				if (!isArray(active) || active.length < 1 || 2 < active.length) {
+					throw new Error('Error in .promise parameter');
+				}
+
+				sql = active[0];
+				params = active[1] || undefined;
+
+				execArray.push(promiseExec(sql, params, i, sqlParamsArray.length));
+			}
+
+			return Promise.all(execArray);
+		};
+
+		alasql.promise = function (sql, params) {
+			if (typeof Promise === 'undefined') {
+				throw new Error('Please include a Promise/A+ library');
+			}
+
+			if (typeof sql === 'string') {
+				return promiseExec(sql, params);
+			}
+
+			if (!isArray(sql) || sql.length < 1 || typeof params !== 'undefined') {
+				throw new Error('Error in .promise parameters');
+			}
+			return promiseAll(sql);
+		};
+	}
+
+	alasql = alasql || false;
+
+	if (!alasql) {
+		throw new Error('alasql was not found');
+	}
+
+	alasql.worker = function () {
+		throw new Error('Can find webworker in this enviroment');
+	};
+
+	if (typeof Worker !== 'undefined') {
+		alasql.worker = function (path, paths, cb) {
+			//	var path;
+			if (path === true) {
+				path = undefined;
+			}
+
+			if (typeof path === 'undefined') {
+				var sc = document.getElementsByTagName('script');
+				for (var i = 0; i < sc.length; i++) {
+					if (sc[i].src.substr(-16).toLowerCase() === 'alasql-worker.js') {
+						path = sc[i].src.substr(0, sc[i].src.length - 16) + 'alasql.js';
+						break;
+					} else if (sc[i].src.substr(-20).toLowerCase() === 'alasql-worker.min.js') {
+						path = sc[i].src.substr(0, sc[i].src.length - 20) + 'alasql.min.js';
+						break;
+					} else if (sc[i].src.substr(-9).toLowerCase() === 'alasql.js') {
+						path = sc[i].src;
+						break;
+					} else if (sc[i].src.substr(-13).toLowerCase() === 'alasql.min.js') {
+						path = sc[i].src.substr(0, sc[i].src.length - 13) + 'alasql.min.js';
+						break;
+					}
 				}
 			}
-		}
 
-		if (typeof path === 'undefined') {
-			throw new Error('Path to alasql.js is not specified');
-		} else if (path !== false) {
-			var js = "importScripts('";
-			js += path;
-			js +=
-				"');self.onmessage = function(event) {" +
-				'alasql(event.data.sql,event.data.params, function(data){' +
-				'postMessage({id:event.data.id, data:data});});}';
+			if (typeof path === 'undefined') {
+				throw new Error('Path to alasql.js is not specified');
+			} else if (path !== false) {
+				var js = "importScripts('";
+				js += path;
+				js +=
+					"');self.onmessage = function(event) {" +
+					'alasql(event.data.sql,event.data.params, function(data){' +
+					'postMessage({id:event.data.id, data:data});});}';
 
-			var blob = new Blob([js], {type: 'text/plain'});
-			alasql.webworker = new Worker(URL.createObjectURL(blob));
+				var blob = new Blob([js], {type: 'text/plain'});
+				alasql.webworker = new Worker(URL.createObjectURL(blob));
 
-			alasql.webworker.onmessage = function(event) {
-				var id = event.data.id;
+				alasql.webworker.onmessage = function (event) {
+					var id = event.data.id;
 
-				alasql.buffer[id](event.data.data);
-				delete alasql.buffer[id];
-			};
+					alasql.buffer[id](event.data.data);
+					delete alasql.buffer[id];
+				};
 
-			alasql.webworker.onerror = function(e) {
-				throw e;
-			};
+				alasql.webworker.onerror = function (e) {
+					throw e;
+				};
 
-			if (arguments.length > 1) {
-				var sql =
-					'REQUIRE ' +
-					paths
-						.map(function(p) {
-							return '"' + p + '"';
-						})
-						.join(',');
-				alasql(sql, [], cb);
+				if (arguments.length > 1) {
+					var sql =
+						'REQUIRE ' +
+						paths
+							.map(function (p) {
+								return '"' + p + '"';
+							})
+							.join(',');
+					alasql(sql, [], cb);
+				}
+			} else if (path === false) {
+				delete alasql.webworker;
+				return;
 			}
-		} else if (path === false) {
-			delete alasql.webworker;
-			return;
-		}
-	};
-}
+		};
+	}
 
+	/* WebWorker */
+	/** @type {number} */
+	alasql.lastid = 0;
 
-/* WebWorker */
-/** @type {number} */
-alasql.lastid = 0;
+	/** @type {object} */
+	alasql.buffer = {};
 
-/** @type {object} */
-alasql.buffer = {};
+	alasql.worker();
 
-alasql.worker();
-
-return alasql;
-}));
+	return alasql;
+});
