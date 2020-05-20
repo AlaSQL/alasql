974740492492bc323230498de0e873d7a4b23692
Mathias Wulff <m@rawu.dk>
2020-05-20 11:03:52 +0200

    Let ADD COLUMN use the given dbtypeid to fix #1196
    All credits goes to @RB-News-MailAccount
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch develop
    # Your branch is ahead of 'origin/develop' by 1 commit.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   src/64altertable.js
    #
    # Untracked files:
    #	dist/
    #

---
+++ dist/alasql-echo.js
@@ -0,0 +1,18 @@
+// Plugin sample
+
+var yy = alasql.yy;
+
+yy.Echo = function (params) { return yy.extend(this, params); }
+yy.Echo.prototype.toString = function() {
+	var s =  'TEST '+this.expr.toString();
+	return s;
+}
+
+yy.Echo.prototype.execute = function (databaseid, params, cb) {
+//	var self = this;
+	// console.log(76336,this.expr.toJS());
+	var fn = new Function('params, alasql','return '+this.expr.toJS());
+	var res = fn(params, alasql);
+	if(cb) res = cb(res);
+	return res;
+}
