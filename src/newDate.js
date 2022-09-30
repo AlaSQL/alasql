8132d54f9766c5b2a714e7d7f9f98a15f8f699ef
Mathias Wulff <m@rawu.dk>
2022-09-30 23:58:20 +1000

    Update newDate.js

--- src/newDate.js
+++ src/newDate.js
@@ -1,9 +1,1 @@
-var dateRegexp = /^\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}\.\d+$/;
-function newDate(d) {
-    if (typeof d === "string") {
-        if (dateRegexp.test(d)) {
-            d = d.replace(".", "-").replace(".", "-");
-        }
-    }
-    return new Date(d);
-}
+
