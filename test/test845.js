d73ce3951a4b7503636130b9febc2b9b035099fd
asundukov <asundukov@stiltsoft.com>
2022-10-20 12:39:48 +0300

    Fix newDate() and Now() reverted back the date format
    newDate() for Safari (et. al.)
    Supported formats:
    "2022.01.10 04:10",
    "2022.01.10 04:10:11",
    "2022.01.10 04:10:11.123",
    "2022-01-10 04:10",
    "2022-01-10 04:10:11",
    "2022-01-10 04:10:11.123",
    "2022.1.10",
    "2022-1-1"
    ...
    
    Fix for NOW() in Firefox: reverted back the date format: 2022-11-01 -> 2022.11.01

--- test/test845.js
+++ test/test845.js
@@ -12,8 +12,8 @@ var test = '845'; // insert test file number
 describe('Test ' + test + ' - use NOW() function', function () {
 	it('1. NOW()', function () {
 		var res = alasql('SELECT NOW() AS now');
-		//2022-02-25 19:21:27.839
-		assert(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}/.test(res[0].now));
+		//2022.02.25 19:21:27.839
+		assert(/\d{4}.\d{2}.\d{2} \d{2}:\d{2}:\d{2}.\d{3}/.test(res[0].now));
 	});
 
 	it('2. CONVERT with NOW() as an argument', function () {
