6cec528ab6122e5ffa6db57567f1823a0bc1f03c
M. Wulff <m@rawu.dk>
2022-12-06 22:53:40 +1100

    Revert "Sum, max, min null value fix on non numeric column (#1537)"
    This reverts commit 7e87d661d3c5f9c1eb8b752b5e2c6ed9a1f49ffb.

--- src/38query.js
+++ src/38query.js
@@ -148,10 +148,8 @@ function queryfn3(query) {
 			if (query.selectGroup.length > 0) {
 				//				console.log(query.selectGroup);
 				query.selectGroup.forEach(function (sg) {
-					if (sg.aggregatorid == 'COUNT') {
+					if (sg.aggregatorid == 'COUNT' || sg.aggregatorid == 'SUM') {
 						g[sg.nick] = 0;
-					} else if (sg.aggregatorid == 'SUM') {
-						g[sg.nick] = 'NULL';
 					} else {
 						g[sg.nick] = undefined;
 					}
