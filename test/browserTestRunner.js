21117291dd1dc4047aa89dbcca2d202b5597bc2b
M. Wulff <m@rawu.dk>
2022-12-20 03:05:11 +1100

    Revert breaking change of node engine

--- test/browserTestRunner.js
+++ test/browserTestRunner.js
@@ -32,49 +32,48 @@
 
 	const testFiles = walkFiles(__dirname, /test\d{3}\.js$/, null, true, true);
 
-	const testFilesHtml = testFiles
-		.map(file => `<script src="${file}"></script>`).join('\n');
+	const testFilesHtml = testFiles.map((file) => `<script src="${file}"></script>`).join('\n');
 
 	var html = template.replace('@INSERT_TESTFILES', testFilesHtml);
 
 	// Server from https://gist.github.com/ryanflorence/701407
-	http.createServer((request, response) => {
-		let pathname = new URL(`http://localhost/` + request.url).pathname;
+	http
+		.createServer((request, response) => {
+			let pathname = new URL(`http://localhost/` + request.url).pathname;
 
-		// normalize leading slash
-		pathname = '/' + pathname.replace(/^\/+/, '');
+			// normalize leading slash
+			pathname = '/' + pathname.replace(/^\/+/, '');
 
-		var filename = path.join(__dirname, pathname);
+			var filename = path.join(__dirname, pathname);
 
-		// all subfolder paths starts from ../ folder
-		if (2 < pathname.split('/').length) {
-			filename = path.join(__dirname + '/../', pathname);
-		}
+			// all subfolder paths starts from ../ folder
+			if (2 < pathname.split('/').length) {
+				filename = path.join(__dirname + '/../', pathname);
+			}
 
-		if ('/' === pathname) {
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write(html);
-			response.end();
-			return;
-		}
+			if ('/' === pathname) {
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.write(html);
+				response.end();
+				return;
+			}
 
-		const exists = fs.existsSync(filename);
+			const exists = fs.existsSync(filename);
 
-		if (!exists || fs.statSync(filename).isDirectory()) {
-			response.writeHead(404, {'Content-Type': 'text/plain'});
-			response.write('404 Not Found\n');
-			response.end();
-			return;
-		}
+			if (!exists || fs.statSync(filename).isDirectory()) {
+				response.writeHead(404, {'Content-Type': 'text/plain'});
+				response.write('404 Not Found\n');
+				response.end();
+				return;
+			}
 
-		response.writeHead(200);
-		fs.createReadStream(filename).pipe(response);
-	})
-	.listen(port);
+			response.writeHead(200);
+			fs.createReadStream(filename).pipe(response);
+		})
+		.listen(port);
 
 	console.log(`Ready to test AlaSQL in the browser at\n  => http://localhost:${port}`);
 	console.log('CTRL + C to shutdown');
 
 	exec(`${process.platform === 'win32' ? 'start' : 'open'} http://localhost:${port}`);
-
 }
