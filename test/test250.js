63595f235ab93ef98880f08e6d2c4c187c6e4f6b
M. Wulff <m@rawu.dk>
2022-12-27 01:10:19 +1100

    Trigger cicd

--- test/test250.js
+++ test/test250.js
@@ -7,8 +7,7 @@ if (typeof exports === 'object') {
 
 describe('Test 250 Index problem', function () {
 	it('1. INSERT SELECT problem', function (done) {
-		alasql(function () {
-			/*
+		alasql(`
       CREATE TABLE tab0(pk INTEGER NOT NULL PRIMARY KEY, col0 INTEGER, col1 FLOAT, col2 TEXT, col3 INTEGER, col4 FLOAT, col5 TEXT);
       INSERT INTO tab0 VALUES(0,6,4.67,'wdbsg',4,2.89,'altmp');
       INSERT INTO tab0 VALUES(1,5,4.67,'wdbsg',4,2.22,'altmp');
@@ -16,8 +15,7 @@ describe('Test 250 Index problem', function () {
       CREATE INDEX idx_tab1_4 on tab1 (col4);
       INSERT INTO tab1 SELECT * FROM tab0;
       DELETE FROM tab1 WHERE col4 > 2.27;
-      */
-		});
+      `);
 
 		var res = alasql('SELECT * from tab1');
 
