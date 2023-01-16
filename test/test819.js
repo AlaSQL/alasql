01599f04b5ad5814f3bbffae4ea870686ed3a554
asundukov <asundukov@stiltsoft.com>
2023-01-16 20:02:06 +0300

    format

--- test/test819.js
+++ test/test819.js
@@ -6,21 +6,18 @@ if (typeof exports === 'object') {
 describe('Test 819 GROUP BY with CASE', function () {
 	it('1. Use GROUP BY with CASE with IS NULL', function (done) {
 		var data = [
-			{id: "id1", alternativeId: undefined},
-			{id: "id2", alternativeId: undefined},
-			{id: "id2", alternativeId: undefined},
-			{id: undefined, alternativeId: "id2"},
-			{id: undefined, alternativeId: "id3"},
+			{id: 'id1', alternativeId: undefined},
+			{id: 'id2', alternativeId: undefined},
+			{id: 'id2', alternativeId: undefined},
+			{id: undefined, alternativeId: 'id2'},
+			{id: undefined, alternativeId: 'id3'},
 		];
 
-		var res = alasql('SELECT COUNT(*) FROM ? GROUP BY CASE WHEN id IS NULL THEN alternativeId ELSE id END', [data]);
-		assert.deepEqual(res, [
-			{'COUNT(*)': 1},
-			{'COUNT(*)': 3},
-			{'COUNT(*)': 1},
-		]);
+		var res = alasql(
+			'SELECT COUNT(*) FROM ? GROUP BY CASE WHEN id IS NULL THEN alternativeId ELSE id END',
+			[data]
+		);
+		assert.deepEqual(res, [{'COUNT(*)': 1}, {'COUNT(*)': 3}, {'COUNT(*)': 1}]);
 		done();
 	});
 });
-
-
