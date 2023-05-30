775e7ae1bee6e6f3466b4260ecf01113b07286b5
M. Wulff <m@rawu.dk>
2023-05-30 13:07:24 +1000

    Include all module files in commits

--- src/15utility.js
+++ src/15utility.js
@@ -1287,7 +1287,7 @@ var getXLSX = function () {
 
 	if (utils.isNode || utils.isBrowserify || utils.isMeteorServer) {
 		//*not-for-browser/*
-		XLSX = require('../modules/xlsx/xlsx.js') || null;
+		XLSX = require('../modules/xlsx/xlsx') || null;
 		alasql.private.externalXlsxLib = XLSX;
 		//*/
 	}
