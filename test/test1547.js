7a4caae29e5f9d0afd73c1b0e737b538ad223fc5
Ali Ahmed Khan <aleee.khaann@gmail.com>
2023-08-04 13:00:04 +0500

    Update testcase test1547.js

--- test/test1547.js
+++ test/test1547.js
@@ -19,13 +19,46 @@ describe('Test 1547 - Empty recordset', function () {
 		alasql('create table one (a int)');
 		alasql('insert into one values (1),(2),(3),(4),(5)');
 		let res = alasql('recordset of select * from one where a = 999');
-		assert.deepEqual(res, {
-			columns: [
-				{
-					columnid: 'a',
-				},
-			],
-			data: [],
-		});
+		// assert.deepEqual(res, {
+		// 	columns: [
+		// 		{
+		// 			columnid: 'a',
+		// 		},
+		// 	],
+		// 	data: [],
+		// });
+
+		assert(
+			(() => {
+				const testdata = {
+					columns: [
+						{
+							columnid: 'a',
+						},
+					],
+					data: [],
+				};
+
+				if (
+					res.columns.length !== testdata.columns.length ||
+					res.data.length !== testdata.data.length
+				) {
+					return false;
+				} else {
+					let cols = res.columns;
+
+					for (let col of cols) {
+						let index = testdata.columns.findIndex((c) => c.columnid === col.columnid);
+
+						if (index === -1) {
+							return false;
+						}
+					}
+
+					return true;
+				}
+			})(),
+			'Response should not have missing coloumns'
+		);
 	});
 });
