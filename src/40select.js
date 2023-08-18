96a3f787d09700d60f6a437036830ad7a236e794
Mathias Wulff <m@rawu.dk>
2023-08-18 17:24:32 +1000

    Update 40select.js

--- src/40select.js
+++ src/40select.js
@@ -503,7 +503,8 @@ function modify(query, res) {
 			columns = [];
 			if (query && query.sources) {
 				query.sources.forEach((source) => {
-					columns = columns.concat((({columnid}) => ({columnid}))(source.columns))
+					if(source?.columns?.columnid)
+						columns = columns.concat((({columnid}) => ({columnid}))(source.columns));
 				})
 			}
 		}
