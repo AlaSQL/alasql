4d405cf982e5cd070ac42e5fd2e2f9843e8cc6e8
rkraja <ramesh.rajagopalan@bayer.com>
2023-09-12 07:28:17 -0500

    improve spec

--- test/test1734.js
+++ test/test1734.js
@@ -11,13 +11,29 @@ describe('Test'  + test + 'Newline characters in like', function () {
 		var data = [
 			{a: 'one', b: 'first'},
 			{a: 'two', b: 'second\n\ritem'},
-			{a: 'THREE', b: 'THIRD'},
+			{a: 'THREE', b: 'THI\n\rRD'},
 		];
 
-		var res = alasql('SELECT b FROM ? WHERE a LIKE "T%"', [data]);
+		var res = alasql('SELECT b FROM ? WHERE b LIKE "t%"', [data]);
 
 		//console.log(res);
-		assert.deepEqual(res, [{b: 'second\n\ritem'}, {b: 'THIRD'}]);
+		assert.deepEqual(res, [{b: 'THI\n\rRD'}]);
+		done();
+	});
+
+	it('2. LIKE', function (done) {
+		var data = [
+			{a: 'one', b: 'Nine'},
+			{a: 'two', b: 'second\n\ritem'},
+			{a: 'THREE', b: 'THIRD'},
+			{a: 'FOUR', b: '\n\rFifth'},
+			{a: 'FIVE', b: 'Six\nth'},
+		];
+
+		var res = alasql('SELECT b FROM ? WHERE b LIKE "%T%"', [data]);
+
+		//console.log(res);
+		assert.deepEqual(res, [{b: 'second\n\ritem'}, {b: 'THIRD'}, {b: '\n\rFifth'}, {b: 'Six\nth'}]);
 		done();
 	});
 });
