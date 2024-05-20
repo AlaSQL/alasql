6f978ba24f87c619d2a2f40e81fb931afe2eb88b
Kamil Figiela <kamil.figiela@gmail.com>
2024-05-20 09:25:45 +0200

    Refactor JSONL parser, add NDJSON as an alias. Fix loadFile error handling for JSON as well.

--- src/84from.js
+++ src/84from.js
@@ -131,28 +131,46 @@ alasql.from.JSON = function (filename, opts, cb, idx, query) {
 		if (cb) {
 			res = cb(res, idx, query);
 		}
-	});
+	}),
+		err => {
+			throw new Error(err);
+		};
 	return res;
 };
 
-alasql.from.JSONL = function (filename, opts, cb, idx, query) {
-	var res;
-	filename = alasql.utils.autoExtFilename(filename, 'jsonl', opts);
-	alasql.utils.loadFile(filename, !!cb, function (data) {
-		res = data.split(/\r?\n/);
-		// Remove last line if empty
-		if (res[res.length - 1] === '') {
-			res.pop();
-		}
-		for (var i = 0, ilen = res.length; i < ilen; i++) {
-			res[i] = JSON.parse(res[i]);
-		}
-		if (cb) {
-			res = cb(res, idx, query);
-		}
-	});
-	return res;
+const jsonl = ext => {
+	return function (filename, opts, cb, idx, query) {
+		let out = [];
+		filename = alasql.utils.autoExtFilename(filename, ext, opts);
+		alasql.utils.loadFile(
+			filename,
+			!!cb,
+			function (data) {
+				data.split(/\r?\n/).forEach((line, ix) => {
+					const trimmed = line.trim();
+					if (trimmed !== '') {
+						// skip empty lines, we do not use filter on an input, as we want to preserve line numbers
+						try {
+							out.push(JSON.parse(trimmed));
+						} catch (e) {
+							throw new Error(`Could not parse JSON at line ${ix}: ${e.toString()}`);
+						}
+					}
+				});
+				if (cb) {
+					res = cb(out, idx, query);
+				}
+			},
+			err => {
+				throw new Error(err);
+			}
+		);
+		return out;
+	};
 };
+
+alasql.from.JSONL = jsonl('jsonl');
+alasql.from.NDJSON = jsonl('ndjson');
 
 alasql.from.TXT = function (filename, opts, cb, idx, query) {
 	var res;
